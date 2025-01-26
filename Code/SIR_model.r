cdc63a9d7f18006fc6396744696cbb2edcf6a141
Omar <umarallwazzani@gmail.com>
2025-01-25 19:21:26 -0500

    removed invorrect  code

--- Code/SIR_model.r
+++ Code/SIR_model.r
@@ -1,12 +1,13 @@
-sf_data <- read.csv("""/Users/omarafifi/MyFolders/Differential-Game-Theory-for-
-                    SIR-Models/Data/san_francisco.csv""")[631:731,]
+load(ggplot)
 
+sf_data <- read.csv("/Users/omarafifi/MyFolders/Differential-Game-Theory-for-SIR-Models/Data/san_francisco.csv")
+sf_data <- sf_data[631:731,]
 
 N = 810000 #population size in 2021
- #percentage of infected individuals in 2021
-percent_infected = sf_data['pos']/sf_data['tests']
+#percentage of infected individuals in 2021
+ 
+I = N*sf_data['pct']
 
-print(percent_infected)
 
 
 
