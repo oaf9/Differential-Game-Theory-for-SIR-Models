cccee7316df78ba81d7ce4ef2bd45d66850fb391
Omar <umarallwazzani@gmail.com>
2025-02-02 14:58:35 -0500

    fixing integration

--- Code/Integrator.R
+++ Code/Integrator.R
@@ -2,32 +2,35 @@
 # set a time sequence ... were doing this over 
 # the length of our original interval
 t = 22
+times <- seq(1, t, by = 1)
 
-times <- seq(0, t, by = 1)
-
-S_0 <- S[1:1,]
-I_0 <- I[1:1,]
-R_0 <- R[1:1,]
+#get initial values from the data
+S_0 <- as.numeric(S$positive[1])
+I_0 <- as.numeric(I$recovered[1])
+R_0 <- as.numeric(R$recovered[1])
 
 #initial values and paramaters must be vectorized
-V_0 <- c(as.numeric(S_0), as.numeric(I_0), as.numeric(R_0))
+state <- c(S = S_0, I = I_0, R = R_0)
 
-#deSolve is very sensitive to the types of the parmamaters.
-paramaters <- c(beta = as.numeric(beta), 
-                gamma = as.numeric(3*gamma), 
-                N = as.numeric(N))
+# deSolve is very sensitive to the types of the parmamaters.
+# since beta is estimated in terms of days, it has to be scaled since we are 
+# predicting in terms of weeks
+paramaters <- c(b = as.numeric(7*beta), 
+                g = as.numeric(gamma), 
+                n = as.numeric(N))
+
 
 #this function describes the dynamic of the system w.r.t. t
 SIR <- function(t, state, paramaters){
   with (as.list(c(state, paramaters)),{
     
-      dS <- (-beta/N)*I*S
-      dI <- (beta/N)*I*S - gamma*I
-      dR = gamma*I
-      
+      dS <- (-b/n)*I*S
+      dI <- (b/n)*I*S - g*I
+      dR = g*I
+    
       #R will automatically return the last last line in the function body
       #so no return keyword is required
-      list(c(dS,dI,dR))
+      return(list(c(dS, dI, dR)))
     })
 }
 
@@ -36,28 +39,53 @@ library(deSolve)
 require(deSolve)
 
 #The equations must be integrated through time. 
-predictions <- ode(y = V_0, times = times, func = SIR, parms = paramaters)
+predictions <- ode(y = state, times = times, func = SIR, parms = paramaters)
 colnames(predictions) <- c("time", "S", "I", "R")      
-        
+
+#Creating a datetime vector for plotting the results
+start_time <- as.Date("2020-10-06")
+date_times <- start_time + (7*times)
+
 #plot the results 
 library(ggplot2)
 require(ggplot2)
 
-SIR_pred <- ggplot(data = predictions, aes(x = times))+
-  geom_line(aes(y = R), 
-            size = 1,
-            color = "#D5C36C")+
-  geom_line(aes(y = S), 
-            size = 1,
-            color = "#C0E4ED")+ 
-  geom_line(aes(y = I), 
-            size = 1,
-            color = "#397689")+
+#
+SIR_pred <- ggplot(data = predictions, aes(x = date_times))+
+  geom_line(aes(y = R, color = "Recovered"), 
+            size = 2)+
+  geom_line(aes(y = S, color = "Susceptible"), 
+            size = 2)+ 
+  geom_line(aes(y = I, color = "Infected"), 
+            size = 2)+
   labs(title = 'SIR Predictions', 
        subtitle = 'Population = 10.2M',
        x = "Date",
-       y = "Number of People")
+       y = "Number of People", 
+       color = "Category")+ 
+   scale_color_manual(values = c("Recovered" = "#D5C36C", 
+                                 "Susceptible" = "#C0E4ED", 
+                                 "Infected" = "#397689"))
 
 #display results
 SIR_pred
+
+
+#plot the difference between actual and predicted values: 
+#lets create a New data frame with real and predicted values: 
+output <- cbind(I, predictions)
+#claculate RSS
+
+ggplot(data = output, aes(x = date_times))+
+  geom_line(aes(y = I, color = "Predicted Counts"), 
+  size = 2, linetype  = 'dashed')+
+  geom_line(aes(y = recovered, color = "Actual Infection Counts"), 
+            size = 2)+ 
+  scale_color_manual(values = c("Predicted Counts" = "red", 
+                                "Actual Infection Counts" = "black"))+ 
+  labs(title = "Predicted vs. Actual Infections", 
+       subtitle = TeX("Paramaters: $ \\beta = .784, \\gamma = .107, N = 10.2M, 
+                      I_0 = 44,357, S_0 = 8,761,220, R_0 = 995$"),
+       x = "Month",
+       y = "Number of Infections")
 
--- Code/SIR_model.r
+++ Code/SIR_model.r
@@ -13,7 +13,7 @@ print('DATA LOADED Successfully')
 
 michigan_data <- michigan_data[c(seq(220, nrow(michigan_data), 7)),]
 N <- 10200000 #population estimate in 2021 
-R <- 10*michigan_data["recovered"]
+R <- michigan_data["recovered"]*10
 S <- N - michigan_data["positive"]*10
 I <- N - R - S
 
@@ -26,20 +26,20 @@ michigan_data["log_infected"] <- log(I)
 
 
 sir_data_plot <- ggplot(data = michigan_data, aes(x = date))+
-  geom_line(aes(y = Recovered), 
-            size = 1,
-            color = "#D5C36C")+
-  geom_line(aes(y = Susceptible), 
-            size = 1,
-            color = "#C0E4ED")+ 
-  geom_line(aes(y = Infected), 
-            size = 1,
-            color = "#397689")+
+  geom_line(aes(y = Recovered, color = "Recovered"), 
+            size = 2)+
+  geom_line(aes(y = Susceptible, color = "Susceptible"), 
+            size = 2)+ 
+  geom_line(aes(y = Infected, color = "Infected"), 
+            size = 2)+
   scale_x_date(date_breaks = "1 month")+
   labs(title = 'SIR Data For The State of Michigan', 
        subtitle = 'Population = 10.2M',
        x = "Date",
-       y = "Number of People")
+       y = "Number of People")+ 
+  scale_color_manual(values = c("Recovered" = "#D5C36C", 
+                                "Susceptible" = "#C0E4ED", 
+                                "Infected" = "#397689"))
 
 sir_data_plot
 
@@ -50,7 +50,7 @@ ggplot(data= michigan_data[1:10,], aes(x = date, y = log_infected))+
   geom_point(size = 3,
              color = "#C0E6ED")+
   geom_smooth(method = "lm",
-              size = 1,
+              size = 2,
               color = "#397689", 
               se = 0)+
   labs(title = TeX("Regression Line for Estimating $\\beta - \\gamma$"), 
