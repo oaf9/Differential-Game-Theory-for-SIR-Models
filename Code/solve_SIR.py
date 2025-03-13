ca049f3008f28b0e3bc803f39992849bb1446282
Omar <umarallwazzani@gmail.com>
2025-03-12 21:32:01 -0400

    message

--- Code/solve_SIR.py
+++ Code/solve_SIR.py
@@ -50,7 +50,7 @@ class solveSIR:
                       t = self.time_steps)
 
 
-    #the backwards equtions
+    #the backwards eqautions
     def lamdaDE(self, t, λ):
         """
         solve the backwards problem for λ(t)
@@ -61,20 +61,21 @@ class solveSIR:
         t = int(t)
         I, I_hat, S_hat, N = self.I[t], self.I_hat[t], self.S_hat[t], self.N
  
-        df_dx = 2*np.array([0, I - I_hat, 0])
-        dh_dx = np.array([[β*I_hat/N  ,  β*S_hat/N, 0],
+        df_dx = -2*np.array([0, I - I_hat, 0])
+        dh_dx = np.array([[β*I_hat/N  ,      β*S_hat/N, 0],
                           [-β*I_hat/N , -β*S_hat/N + γ, 0],
-                          [0          ,         -γ, 0]])
+                          [0          ,             -γ, 0]])
 
-        #return the negative since we are moving backwards
+        #return the negative since we are integrating backwards
+
         return  -(df_dx + np.array(λ).T@dh_dx)
     
     
     def backwardsSolve(self):
         """Backwards solve for λ(t). 
         """
-        #we have to integrate backwards
-        #output must be flipped since odeint will sovle a forward problem.
+
+        #output must be flipped since the solver will only solve a forward problem.
         return solve_ivp(fun = self.lamdaDE, 
                         y0 = [0,0,0], 
                         t_span = [0, self.time_steps[-1]],
@@ -105,20 +106,20 @@ class solveSIR:
                            [0,0,0,0,1]])
  
         #we get all the timesteps: 
-        integrands = [self.integrand(t, self.λ) for t in self.time_steps]
+        integrands = np.array([self.integrand(t, self.λ) for t in self.time_steps])
 
         #integrate with simpson's method and add the gx(0)gp term 
         return (simpson(y = integrands, x = self.time_steps, axis = 0) + 
                 self.λ[0].T@dh_dx_dt@ np.linalg.inv(dg_dx0)@dg_dp)
     
 
-    def fit(self, max_iter = 100, η = .01, ε = .01):
+    def fit(self, max_iter = 100, η = .001, ε = .01):
         """A gradient descent implementation to find p*
         """
         i = 0
         while(True):
 
-            print(self.p)
+            print(f"round: {i}")
 
             #step 1 is to integrate for (S,I,R) and update the SIR values
             V = self.forwardSolve()
@@ -127,21 +128,27 @@ class solveSIR:
             self.I_hat = V[:, 1]
             self.R_hat = V[:, 2]
 
+
             #step 2 is to solve the backwards problems 
             self.λ = self.backwardsSolve()
 
-            print("lambda ", self.λ)
+            print(np.max(self.λ))
+
 
             #compute the gradient
-            dL_dp = np.clip(self.dF_dp(),-1e3, 1e3)
+            dL_dp = np.clip(self.dF_dp(),-1, 1)
+            dL_dp = self.dF_dp()
 
+            
             #perform the gradient update
             self.p = self.p - η*dL_dp
 
+
             #check for convergence: 
-            if( (np.sqrt(np.dot(self.p, self.p)) < ε)  or i > max_iter):
+            if( (np.sqrt(np.dot(dL_dp,  dL_dp)) < ε)  or i > max_iter):
                 break;
             else:
                 i += 1
+
     
         return self.p, self.forwardSolve()
