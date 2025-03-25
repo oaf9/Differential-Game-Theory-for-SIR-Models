e177f44512a13e6169c2455cda60b7f8faec37f9
Omar <umarallwazzani@gmail.com>
2025-03-25 00:05:42 -0400

    implemented controls

--- Code/Control_Model/control_model.py
+++ Code/Control_Model/control_model.py
@@ -7,38 +7,61 @@ def solveQuadratic(a,b,c):
     """ Solves ax^2 + bx +c = 0
         Note: this solver assumes the existence of two real roots
     """
-    x1 = -b + np.sqrt(b*b - 4*a*c)
-    x2 = -b - np.sqrt(b*b - 4*a*c)
+    #a,b,c = a.detach(), b.detach(), c.detach()
+    x1 = -b + torch.sqrt(b*b - 4*a*c)
+    x2 = -b - torch.sqrt(b*b - 4*a*c)
 
-    return 2*a*np.array(x1,x2)
+    #print(torch.sqrt(b*b - 4*a*c))
 
+    return torch.stack([x1,x2])/(2*a)
 
 class control_model(torch.nn.Module):
-    def __init__(self, θ):
+    def __init__(self, θ, m):
         super(control_model, self).__init__()
 
-        """Constructor: θ = (Β_0, γ_0, m,  S_0, I_0, V_0)
+        """Constructor: θ = (Β_0, γ_0,  m,  S_0, I_0, V_0)
         """
-        self.θ = torch.nn.Parameter(θ) 
+        self.θ = torch.nn.Parameter(θ)
+        self.m = torch.nn.Parameter(m)
+
+        self.cost = []
+
+    def σ(self, c):
+        """ models the returns the risk of infection in terms of control"""
+        return 1/(self.m*c + 1)
 
     def forward(self, t, X):
+
         # defines the SIR equations
         Β, γ  = self.θ[0:2]
-        S, I, R = X
 
+        S, I, R, Vs = X
         N = S + I + R
 
-        dS = -(Β/N)*S*I
-        dI = (Β/N)*S*I - γ*I
-        dR = γ*I
+        #update the control values
+        a,b,c = (self.m*self.m), 2*self.m, (1-self.m*I*(1+Vs))
 
-        return torch.stack([dS, dI, dR])
+        c_optimal = 0
+
+        if c >= 0: 
+            c_optimal = solveQuadratic(a,b,c)[0]
+
+        σ_c = self.σ(c_optimal)
+
+        
+        dS = -σ_c*(Β/N)*S*I
+        dI = σ_c*(Β/N)*S*I - γ*I
+        dR = γ*I
+        dVs = (1+Vs)* σ_c * I + c_optimal
+
+        return torch.stack([dS, dI, dR, -dVs])
+    
+
     
     def get_initial_conditions(self):
 
         #this function is just a helpful getter ... could be removed without 
         #affecting anything except some syntactical stuffs.
-
         return self.θ[2:]
     
 
--- Code/Control_Model/control_solver.py
+++ Code/Control_Model/control_solver.py
@@ -1,4 +1,4 @@
-from Code.SIR_adjoint_model import SIR
+from control_model import control_model
 from torchdiffeq import odeint_adjoint as odeint
 import torch
 
@@ -6,23 +6,27 @@ import torch
 def lossF(pred, true): 
         return torch.mean((pred - torch.tensor(true, dtype=torch.float32))**2)
 
-def fit(p_0, I, epochs = 100, lr = .001):
+def fit(p_0, m, I, epochs = 100, lr = .001):
 
     #first we ensure that the initial paramaters are compatible with torch
-    # p_0 = (Β_0, γ_0, S_0, I_0, R_0 ) -- matches params for SIR()
+    # p_0 = (Β_0, γ_0, S_0, I_0, R_0, V_0 ) -- matches params for SIR()
     p_0 = torch.tensor(p_0, dtype = torch.float32)
+    m = torch.tensor(m, dtype = torch.float32)
+    
     N = p_0[2:].sum()
 
     #setting the timesteps to fit at ... this will depend on your data
     t = torch.linspace(0, len(I), 152)
 
     #initilize the model
-    model = SIR(p_0)
+    model = control_model(p_0, m)
 
     #using stochastic gradient descent 
     optimizer = torch.optim.SGD(model.parameters(), lr = .001)
 
     for i in range(epochs): 
+
+        print(model.m)
 
         #make the forward pass: 
 
@@ -33,20 +37,28 @@ def fit(p_0, I, epochs = 100, lr = .001):
         #calculate loss
         loss = lossF(I_hat, I)
 
+        if(loss < .001): 
+             break
+
         #compute and update the gradients
         loss.backward()
         optimizer.step()
 
         #project the values back into valid numerical ranges
         with torch.no_grad():
-            model.θ.data[0] = model.θ.data[0].clamp(0.001,5)
-            model.θ.data[1] = model.θ.data[1].clamp(0.001,1)
-            model.θ.data[2:] = model.θ.data[2:].clamp(0.001, N)
+
+            model.θ.data[0] = model.θ.data[0].clamp(0.0001,5)
+            model.θ.data[1] = model.θ.data[1].clamp(0.0001,1)
+            model.θ.data[2:] = model.θ.data[2:].clamp(0.0001, N)
             model.θ.data[2:] = model.θ.data[2:] / model.θ.data[2:].sum() * N
 
         #print values
-        print(f"p = {model.θ}")
+        print(f"p = {model.θ}, {model.m}")
         print(f"Loss {i} = {loss}")
+
+
+
+
 
 
     # make a forward pass and return those values
